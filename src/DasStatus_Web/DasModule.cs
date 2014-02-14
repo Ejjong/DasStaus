eb816966e53619fe791b0b833b8a83d8c6888b76
Jongho Lee <ejjong7@gmail.com>
2014-02-14 09:26:54 +0900

    add nancy

---
+++ src/DasStatus_Web/DasModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DasStatus_Web
+{
+    public class DasModule : NancyModule
+    {
+        public DasModule()
+        {
+            Get["/status"] = _ => "Hello World";
+        }
+    }
+}
