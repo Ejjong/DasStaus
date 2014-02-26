0f6d8a27306cc880c27192ae727d40c7d9153f7d
Jongho Lee <ejjong7@gmail.com>
2014-02-26 14:33:30 +0900

    remove duplicate logic

--- src/DasStatus_Web/Config.cs
+++ src/DasStatus_Web/Config.cs
@@ -19,27 +19,12 @@ namespace DasStatus_Web
             if (String.Equals(fromConfig, "{ENV}", StringComparison.InvariantCultureIgnoreCase))
             {
                 var ret = Environment.GetEnvironmentVariable(key);
-                if (ret.Contains("postgre")) return GetP(ret);
                 return ret;
             }
             else
             {
                 return fromConfig;
             }
-        }
-
-        public string GetP(string postgreUrl)
-        {
-            var uriString = postgreUrl;
-            var uri = new Uri(uriString);
-            var db = uri.AbsolutePath.Trim('/');
-            var user = uri.UserInfo.Split(':')[0];
-            var passwd = uri.UserInfo.Split(':')[1];
-            var port = uri.Port > 0 ? uri.Port : 5432;
-            var connStr = string.Format("Server={0};Database={1};User Id={2};Password={3};Port={4}",
-                uri.Host, db, user, passwd, port);
-           
-            return connStr;
         }
     }
 
