f5f5344b2c6c6b291a3bc95d75618c8933468aed
Jongho Lee <ejjong7@gmail.com>
2014-02-26 14:39:36 +0900

    fix error

--- src/DasStatus_Web/Utilities.cs
+++ src/DasStatus_Web/Utilities.cs
@@ -24,12 +24,12 @@ namespace DasStatus_Web
             IOrmLiteDialectProvider provider = null;
             if (ConnectionString.StartsWith("postgres://"))
             {
-                connString = GenerateConnectionStringForPostgreSQL(connString);
+                connString = GenerateConnectionStringForPostgreSQL(ConnectionString);
                 provider = PostgreSqlDialect.Provider;
             }
             else if (ConnectionString.StartsWith("sqlserver://"))
             {
-                connString = GenerationConnectionStringForSqlServer(connString);
+                connString = GenerationConnectionStringForSqlServer(ConnectionString);
                 provider = SqlServerDialect.Provider;
             }
             else
